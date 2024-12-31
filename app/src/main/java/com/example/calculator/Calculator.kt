33c3bfda8ef096eb24b0eeedb17058dd0d92ea03
Pallavi Dwivedi <dpallavi854@gmail.com>
2024-12-31 23:26:53 +0530

    Added gradient BG and Styled Buttons as well

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,5 +1,6 @@
 package com.example.calculator
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
@@ -26,6 +28,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
+import androidx.compose.ui.graphics.Brush
 
 val buttonList = listOf(
     "C","(",")","/",
@@ -35,46 +39,59 @@ val buttonList = listOf(
     "AC","0",".","="
 )
 
-
 @Composable
-fun Calculator(modifier: Modifier = Modifier, viewModel: CalculatorViewModel = CalculatorViewModel()){
+fun Calculator(modifier: Modifier = Modifier, viewModel: CalculatorViewModel = CalculatorViewModel()) {
 
     val equationText = viewModel.equationText.observeAsState()
     val resultText = viewModel.resultText.observeAsState()
 
-    Box(modifier = modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF5C774E) , // Green
+                        Color(0xFF0F1D12), // Subtle dark green
+                        Color(0xFF070707), // Dark color
+                    ),
+                    startY = 5f, // Adjust the start position of the gradient
+                )
+            )
+    ) {
         Column(
-            modifier = modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.End,
+            modifier = modifier .fillMaxSize(),
+            horizontalAlignment = Alignment.Start,
         ) {
-            //Input Text Area
+            // Input Text Area
             Text(
-                text = equationText.value?:"",
+                text = equationText.value ?: "",
                 style = TextStyle(
                     fontSize = 30.sp,
-                    textAlign = TextAlign.End,
+                    textAlign = TextAlign.Start,
                 ),
                 maxLines = 5,
                 overflow = TextOverflow.Ellipsis
             )
 
-            //Spacer
+            // Spacer
             Spacer(modifier = Modifier.weight(1f))
 
-            //Output Calculation
+            // Output Calculation
             Text(
-                text = resultText.value?:"",
+                text = resultText.value ?: "",
+                modifier = Modifier.padding(start = 10.dp),
                 style = TextStyle(
                     fontSize = 60.sp,
-                    textAlign = TextAlign.End,
+                    textAlign = TextAlign.Start,
                 ),
                 maxLines = 3
             )
 
-            //Spacer
+            // Spacer
             Spacer(modifier = Modifier.height(10.dp))
 
-            //Keyboard
+            // Keyboard
             LazyVerticalGrid(
                 columns = GridCells.Fixed(4),
                 modifier = Modifier.fillMaxWidth()
@@ -92,22 +109,42 @@ fun Calculator(modifier: Modifier = Modifier, viewModel: CalculatorViewModel = C
 
 //Button Function
 @Composable
-fun CalculatorButton(btn: String, onClick: () -> Unit){
-    Box(modifier = Modifier.fillMaxWidth().padding(8.dp), contentAlignment = Alignment.Center){
-        FloatingActionButton(
-            onClick = onClick,
-            modifier = Modifier.size(80.dp),
-            shape = CircleShape,
-            contentColor = Color.White,
-            containerColor = getColor(btn),
-        ){
-            Text(text = btn,
-                style = TextStyle(
-                    fontSize = 28.sp,
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.Bold
+fun CalculatorButton(btn: String, onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(80.dp)
+
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFF4D4D4D), // Subtle gray border
+                    shape = RoundedCornerShape(15.dp)
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            FloatingActionButton(
+                onClick = onClick,
+                modifier = Modifier.size(80.dp),
+                shape = RoundedCornerShape(15.dp),
+                containerColor = Color(0xFF202020),
+                contentColor = Color.White
+            ) {
+                Text(
+                    text = btn,
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                    style = TextStyle(
+                        fontSize = 28.sp,
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White // Text color
+                    )
                 )
-            )
+            }
         }
     }
 }
@@ -116,6 +153,6 @@ fun CalculatorButton(btn: String, onClick: () -> Unit){
 fun getColor(btn: String): Color {
     if(btn == "=")
         return Color(0xFF5c774e)
-    return Color(0xFF666666)
+    return Color(0xFF202020)
 
 }
