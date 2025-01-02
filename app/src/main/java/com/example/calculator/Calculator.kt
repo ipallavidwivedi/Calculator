1c5b7bf5b652cfbedf93b21ffb82d299dc1f448e
Pallavi Dwivedi <dpallavi854@gmail.com>
2025-01-02 22:19:14 +0530

    just checking

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -46,6 +46,7 @@ fun Calculator(modifier: Modifier = Modifier, viewModel: CalculatorViewModel = C
     val resultText = viewModel.resultText.observeAsState()
 
     Box(
+        //need to change gradient look
         modifier = Modifier
             .fillMaxSize()
             .background(
